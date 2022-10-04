03310e2f8732aa06233d21b11b6ac11bfec3a1ef
Begoña Alvarez <balvarez@boxfish.studio>
2022-10-04 12:57:47 +0200

    devmode-only: hack desktop for mobile development purposes

--- packages/shared/lib/core/router/subrouters/onboarding/app-setup-router.ts
+++ packages/shared/lib/core/router/subrouters/onboarding/app-setup-router.ts
@@ -18,7 +18,7 @@ export class AppSetupRouter extends Subrouter<AppSetupRoute> {
         const currentRoute = get(this.routeStore)
         switch (currentRoute) {
             case AppSetupRoute.Welcome:
-                nextRoute = AppSetupRoute.Legal
+                this.parentRouter.next()
                 break
             case AppSetupRoute.Legal:
                 nextRoute = AppSetupRoute.LanguageAndAppearance
